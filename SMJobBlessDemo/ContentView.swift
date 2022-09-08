0e49dab1cfe8443a8dc6481e2e0aec5f8c0f9b70
IKEH1024 <pondau1024@gmail.com>
2022-09-08 10:13:50 +0900

    initial commit

---
+++ SMJobBlessDemo/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  SMJobBlessDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button {
+//            guard let auth = Util.askAuthorization() else {
+//                fatalError("Authorization not acquired.")
+//            }
+//            Util.blessHelper(label: Constant.helperMachLabel, authorization: auth)
+//            client.start()
+        } label: {
+            Text("Action!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SMJobBlessDemo/SMJobBlessDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SMJobBlessDemoApp.swift
+//  SMJobBlessDemo
+//
+//  Created by HIROKI IKEUCHI on 2022/09/08.
+//
+
+import SwiftUI
+
+@main
+struct SMJobBlessDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(width: 300, height: 200)
+        }
+    }
+}
